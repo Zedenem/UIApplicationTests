2550758b02ebe5e4173386ff53d333433ff5b613
Zouhair Mahieddine <zouhair.mahieddine@gmail.com>
2019-02-26 22:17:41 +0100

    XCode project created

---
+++ TestApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApplication
+//
+//  Created by Zedenem on 26/02/2019.
+//  Copyright © 2019 Zedenem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
